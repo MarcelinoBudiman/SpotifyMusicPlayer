3cc48855d1dd10e52312711f4463edd5cc0df528
MarcelinoBudiman <BATM.MARCELI@xl.co.id>
2025-05-17 10:10:46 +0700

    Initialize spotify API SDK

--- SpotifyMusicPlayer/App/AppDelegate.swift
+++ SpotifyMusicPlayer/App/AppDelegate.swift
@@ -6,33 +6,113 @@
 //
 
 import UIKit
+import SpotifyiOS
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    
+    // MARK: Initializze Spotify Client SDK
+    let SpotifyClientID = "938424997fbb41e095914f59d0038930"
+    let SpotifyRedirectURL = URL(string: "spotify-music-player://spotify-login-callback")!
+
+    lazy var configuration = SPTConfiguration(clientID: SpotifyClientID, redirectURL: SpotifyRedirectURL)
+    
+    lazy var appRemote: SPTAppRemote = {
+      let appRemote = SPTAppRemote(configuration: self.configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = UserDefaults.standard.string(forKey: "accessToken")
+      appRemote.delegate = self
+      return appRemote
+    }()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootVC = UIViewController()
+        
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+        
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let parameters = appRemote.authorizationParameters(from: url)
+        
+
+        if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = access_token
+            
+            UserDefaults.standard.set(access_token, forKey: "accessToken")
+            
+            } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+                // Show the error
+            }
+        
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        if self.appRemote.isConnected {
+            self.appRemote.disconnect()
+        }
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        if let _ = self.appRemote.connectionParameters.accessToken {
+            self.appRemote.connect()
+        }
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//        // Called when a new scene session is being created.
+//        // Use this method to select a configuration to create the new scene with.
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+//
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        // Called when the user discards a scene session.
+//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//    }
+
+
+}
+
+extension AppDelegate: SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: (any Error)?) {
+        print("Failed to connect to spotify!")
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: (any Error)?) {
+        print("Disconnected from spotify!")
     }
-
-
+    
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        print("Connected to spotify!")
+        
+        self.appRemote.playerAPI?.delegate = self
+        
+        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+            if let error = error {
+                debugPrint("Error subscribing to player state = " + error.localizedDescription)
+            }
+      })
+        
+    }
+    
+    func playerStateDidChange(_ playerState: any SPTAppRemotePlayerState) {
+        print("player state changed")
+        
+        debugPrint("Track name: \(playerState.track.name)")
+    }
+    
 }
 
