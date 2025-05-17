5e4553c00bc7e464c1c42ea2d235bcd332524b7f
MarcelinoBudiman <BATM.MARCELI@xl.co.id>
2025-05-17 09:12:01 +0700

    Initial project commit

---
+++ SpotifyMusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyMusicPlayer
+//
+//  Created by Marcelino Budiman on 17/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
