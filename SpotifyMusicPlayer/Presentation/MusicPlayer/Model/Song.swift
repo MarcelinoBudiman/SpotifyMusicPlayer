8a787927d5a4bc93325b72eca3235a0936ae959e
MarcelinoBudiman <BATM.MARCELI@xl.co.id>
2025-05-17 11:41:37 +0700

    Integrate rx swift using SPM, implement song model

---
+++ SpotifyMusicPlayer/Presentation/MusicPlayer/Model/Song.swift
@@ -0,0 +1,17 @@
+//
+//  Song.swift
+//  SpotifyMusicPlayer
+//
+//  Created by Marcelino Budiman on 17/05/25.
+//
+
+import Foundation
+
+struct Song {
+    let imageURL: String
+    let name: String
+    let artist: String
+    let album: String
+    let isPaused: Bool
+    let playbackPosition: Int
+}
