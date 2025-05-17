cecc8b8cc5a0b4bec8724d757e5a60fc6324ef0b
MarcelinoBudiman <BATM.MARCELI@xl.co.id>
2025-05-17 11:16:32 +0700

    Remove default view controller, Create new music player view controller, Create MVVM structure for presentation

--- SpotifyMusicPlayer/App/AppDelegate.swift
+++ SpotifyMusicPlayer/App/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let rootVC = ViewController()
+        let rootVC = MusicPlayerViewController()
         
         window?.rootViewController = UINavigationController(rootViewController: rootVC)
         window?.makeKeyAndVisible()
---
+++ SpotifyMusicPlayer/Presentation/MusicPlayer/View/MusicPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MusicPlayerViewController.swift
+//  SpotifyMusicPlayer
+//
+//  Created by Marcelino Budiman on 17/05/25.
+//
+
+import UIKit
+
+class MusicPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
